351075f0458190e3a7b3b6e01ebf5651699e3f36
Yuzuka4573 <yuto457350@gmail.com>
2018-08-22 11:54:49 +0900

    move project

---
+++ Assets/Scripts/DropAmmo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropAmmo : MonoBehaviour {
+
+    public GameObject AmmoPre;
+    SoundSpawn SS;
+    public bool isShoot=true;
+    public float CTime = 0f;
+    public float limitCT = 0.25f;
+	// Use this for initialization
+	void Start () {
+        SS = GetComponent<SoundSpawn>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && isShoot) {
+            Instantiate(AmmoPre,gameObject.transform.position,gameObject.transform.rotation);
+            isShoot = false;
+            CTime = 0f;
+        }
+        if (!isShoot) CTime += Time.deltaTime;
+        if (CTime > limitCT) isShoot = true;
+	}
+}
---
+++ Assets/Scripts/LaunchBullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchBullet : MonoBehaviour {
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = transform.right * speed;
+    }
+	
+}
